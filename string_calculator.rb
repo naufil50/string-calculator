67e136fd7d8af0edc0ccd5f1f39a0a9f739af517
naufil50 <naufil50@gmail.com>
2024-07-15 01:40:28 +0530

    Add custom delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,27 +6,48 @@ class StringCalculator
     @numbers = numbers
     return 0 if @numbers.empty?
 
-    @delimiters = DEFAULT_DELIMITERS
+    extract_delimiters_and_numbers
     validate_positive_numbers!
     final_result
   end
 
   private
 
-  def numbers_list
-    @_numbers_list ||= @numbers.split(delimiters_regex).map(&:to_i).select { |num| num <= MAX_ALLOWED_NUMBER }
+  def extract_delimiters_and_numbers
+    if @numbers.start_with?("//")
+      @delimiters_section, @numbers = @numbers.split("\n", 2)
+      @delimiters = extract_delimiters
+    else
+      @delimiters = DEFAULT_DELIMITERS
+    end
   end
 
-  def delimiters_regex
-    Regexp.union(@delimiters)
+  def extract_delimiters
+    @delimiters_section.include?("[") ? extract_delimiters : [extract_single_delimiter]
+  end
+
+  def extract_single_delimiter
+    @delimiters_section[2..-1]
+  end
+
+  def extract_delimiters
+    @delimiters_section.scan(/\[([^\]]+)\]/).flatten
   end
 
   def validate_positive_numbers!
     raise negative_numbers_error_text unless negative_numbers.empty?
   end
 
+  def numbers_list
+    @_numbers_list ||= @numbers.split(delimiters_regex).map(&:to_i).select { |num| num <= MAX_ALLOWED_NUMBER }
+  end
+
   def negative_numbers
     @_negative_numbers ||= numbers_list.select { |num| num < 0 }
+  end
+
+  def delimiters_regex
+    Regexp.union(@delimiters)
   end
 
   def negative_numbers_error_text
